2ed918d35089e125812176983b38fcb877194c1a
David Bochenski <david@bochenski.co.uk>
2011-11-06 17:26:49 +0000

    first working version

---
+++ deepbeige/build.sbt
@@ -0,0 +1,5 @@
+name := "DeepBeige"
+
+version := "1.0"
+
+scalaVersion := "2.9.1"
---
+++ tutorial.sh
@@ -0,0 +1,1 @@
+python tools/playgame.py "scala deepbeige/target/scala-2.9.1/deepbeige_2.9.1-1.0.jar" "python tools/sample_bots/python/HunterBot.py" --map_file tools/maps/example/tutorial1.map --log_dir game_logs --turns 60 --scenario --food none --player_seed 7 --verbose -e
