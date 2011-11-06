77d83870b1e828305da2b5e9d9d32709f312b690
David Bochenski <david@bochenski.co.uk>
2011-11-06 18:15:17 +0000

    added crash detection logic

--- deepbeige/src/main/scala/MyBot.scala
+++ deepbeige/src/main/scala/MyBot.scala
@@ -9,16 +9,28 @@ class MyBot extends Bot {
     // Your logic goes here.
     // for example ...
 
+    //
+
     val directions = List(North, East, South, West)
     val ants = game.board.myAnts.values
-    ants.flatMap{ant =>
+  	var new_locations = List[Tile]() 
+		
+	
+  	ants.flatMap{ant =>
       // for this ant, find the first direction which is not water, if any
       val direction = directions.find{aim =>
         val targetTile = game.tile(aim).of(ant.tile)
-        !game.board.water.contains(targetTile)
+        !game.board.water.contains(targetTile) && !new_locations.contains(targetTile)
       }
-      // convert this (possible) direction into an order for this ant
-      direction.map{d => Order(ant.tile, d)}
-    }.toSet
+			new_locations = direction match {
+				case Some(aim) => {
+					new_locations :+ game.tile(aim).of(ant.tile)
+		      // convert this (possible) direction into an order for this ant
+	
+				}
+				case None => new_locations
+			}
+			direction.map{d => Order(ant.tile, d)}
+		}.toSet
   }
 }
