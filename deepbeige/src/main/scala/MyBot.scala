8a3cec95bf4c5d3e941056450b6564f0e4afac00
David Bochenski <david@bochenski.co.uk>
2011-12-02 17:17:37 +0000

    Switched to purely functional style
    not a var in sight

--- deepbeige/src/main/scala/MyBot.scala
+++ deepbeige/src/main/scala/MyBot.scala
@@ -6,49 +6,37 @@ object MyBot extends App {
 class MyBot extends Bot {
 
   def ordersFrom(game: Game): Set[Order] = {
-
-    val directions = List(North, East, South, West)
     val ants = game.board.myAnts.values
     val food = game.board.food.values
+    getFoodOrders(game,ants,food,List[Tile]())			   
+  }
 
-    var new_locations = List[Tile]() 
-    var busyAnts = List[MyAnt]()
-
-    val food_orders = food.flatMap{ morsel =>
-      val gatherer = ants.reduceLeft[MyAnt]{ (ant1 , ant2) =>
+  private def getFoodOrders(game: Game, spare_ants: Iterable[MyAnt], remaining_food: Iterable[Food], new_locations: List[Tile] ) : Set[Order] = {
+    if ((spare_ants.size == 0) || (remaining_food.size == 0)) {
+      Set[Order]()
+    } else { 
+      val morsel = remaining_food.head
+      val gatherer = spare_ants.reduceLeft[MyAnt]{ (ant1 , ant2) =>
 	val ant1_distance = game.distanceFrom(ant1.tile).to(morsel.tile)
 	val ant2_distance = game.distanceFrom(ant2.tile).to(morsel.tile)
         if (ant1_distance < ant2_distance) { ant1 } else { ant2 }						
       }
-				   
-      val direction = directions.filter( d => game.directionFrom(gatherer.tile).to(morsel.tile).contains(d)).find { aim =>
-	val targetTile = game.tile(aim).of(gatherer.tile)
-          !game.board.water.contains(targetTile) && !new_locations.contains(targetTile)													         }
-        new_locations = direction match {
-	  case Some(aim) => { 
-	    new_locations :+ game.tile(aim).of(gatherer.tile)
-	  }
-	  case None => new_locations
-	}
-      direction.map{ d => Order(gatherer.tile,d)}
-    }.toSet
+      val order = getOrder(game,gatherer,morsel,new_locations)
+      System.err.println(order.toString)
+      order._1.toSet ++ getFoodOrders(game,spare_ants.filterNot(x => x == gatherer),remaining_food.filterNot(x => x == morsel),order._2)
+    }
+  }
 
-    System.err.println(food_orders.toString)
-    food_orders
-  //  ants.flatMap{ant =>
-      // for this ant, find the first direction which is not water, if any
-  //    val direction = directions.filter( d => game.directionFrom(onTILE).to(anotherTile).contains(d))find{aim =>
-  //      val targetTile = game.tile(aim).of(ant.tile)
-  //      !game.board.water.contains(targetTile) && !new_locations.contains(targetTile)
-  // /   } 
-  //    new_locations = direction match {
-//	case Some(aim) => {
-//	new_locations :+ game.tile(aim).of(ant.tile)
-        // convert this (possible) direction into an order for this ant
-//	}
-//	case None => new_locations
-//      }
-//      direction.map{d => Order(ant.tile, d)}
-//    }.toSet
+  private def getOrder(game: Game, ant: MyAnt, target: Positionable, locations: List[Tile]) = {
+    val directions = List(North,East,South,West) 
+    val direction: Option[CardinalPoint] = directions.filter( d => game.directionFrom(ant.tile).to(target.tile).contains(d)).find { aim =>
+      val targetTile = game.tile(aim).of(target.tile)
+      !game.board.water.contains(targetTile) && !locations.contains(targetTile) }
+    val order = direction.map{ d => Order(ant.tile,d)}
+    val new_locations = direction match {
+      case Some(aim) => { locations :+ game.tile(aim).of(ant.tile) }
+      case None => locations
+    }
+    (order,new_locations)
   }
 }
