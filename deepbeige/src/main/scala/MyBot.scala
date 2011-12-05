66e8632c0a76b6436479110bf708edc9ca416deb
David Bochenski <david@bochenski.co.uk>
2011-12-05 23:31:51 +0000

    Further Refactorings
    Seems to override food orders, causing oscillation, but otherwise
    looking pretty good.  Liking the refactoring and use of fold left
    instead of explicit recursion

--- deepbeige/src/main/scala/MyBot.scala
+++ deepbeige/src/main/scala/MyBot.scala
@@ -7,7 +7,6 @@ class MyBot extends Bot {
 
   def ordersFrom(game: Game): Set[Order] = {
     val ants = game.board.myAnts.values
-   
     val unexplored = {
       (0 until game.parameters.rows).foldLeft(Set[Tile]()) {(set, row) =>
 	set ++ { 
@@ -19,11 +18,9 @@ class MyBot extends Bot {
     }.&~(game.board.exploredTiles.keys.toSet)
 
     System.err.println("unexplored: " + unexplored.size.toString)
-
     System.err.println("Deep Beige is Hungry")
     val my_hill_tiles = game.board.myHills.values.map { hill => hill.tile }.toList
-
-    val food_orders = getTargetsForAnts(game,ants,game.board.food.keys,my_hill_tiles)
+    val food_orders = getAntsForTargets(game,ants,game.board.food.keys,my_hill_tiles)
     System.err.println("food orders " + food_orders._1.toString)
     System.err.println("remainingAnts after food orders: " + food_orders._2.toString)
     val explore_orders = getTargetsForAnts(game, food_orders._2,unexplored,food_orders._3)
@@ -33,44 +30,56 @@ class MyBot extends Bot {
     food_orders._1 ++ explore_orders._1 ++ left_overs
   }
 
-  private def getLeftOvers(game: Game, available_ants: Iterable[MyAnt], locations: List[Tile] ) : Set[Order] = {
-    if (available_ants.size == 0) { Set[Order]() 
-    } else {
-      val directions = List(North,East,South,West)
-      val ant = available_ants.head
-      
-      val order = getOrder(game,ant,directions,locations)
-      order._1.toSet ++ getLeftOvers(game,available_ants.filterNot(x => x == ant),order._2)
-    }
-  }
+  private def getLeftOvers(game: Game, available_ants: Iterable[MyAnt], blocked_locations: List[Tile] ) : Set[Order] = {
+     available_ants.flatMap(  ant => { 
+      getOrder(game,ant,List(North,East,South,West),blocked_locations)     }).toSet 
+  }			 
 
+  private def getAntsForTargets(game: Game, available_ants: Iterable[MyAnt], targets: Iterable[Tile],
+				blocked_locations: List[Tile]) : (Set[Order], Iterable[MyAnt], List[Tile]) = {
+    val result = targets.foldLeft((Set[Order](),available_ants,blocked_locations,List[MyAnt]())) { (state,target) => {
+      if (state._2 == 0) { state } else {
+	val ant = available_ants.reduceLeft[MyAnt]{(ant1, ant2) => 
+	  if(game.distanceFrom(ant1.tile).to(target) < game.distanceFrom(ant2.tile).to(target)) { ant1 } else { ant2 }
+      }
+      val possible_directions = List(North,East,South,West).filter(d => game.directionFrom(ant.tile).to(target).contains(d))
+      getOrder(game,ant,possible_directions,state._3) match { 
+	case Some(order) => { 
+	  val new_blocked_locations = game.tile(order.point).of(ant.tile) +: state._3
+	  (state._1 + order, state._2.filterNot(_ == ant),new_blocked_locations,ant +: state._4)}
+	case None => state
+      }
+      }
+    }}
+    (result._1,available_ants.filterNot(x => result._4.contains(x)),result._3)											       
+  }			       
+			 
   private def getTargetsForAnts(game: Game, available_ants: Iterable[MyAnt], targets: Iterable[Tile], 
 				blocked_locations: List[Tile]) : (Set[Order], Iterable[MyAnt], List[Tile]) = {
-
     val result = available_ants.foldLeft((Set[Order](),targets,blocked_locations,List[MyAnt]())) { (state,ant) => {
       if (state._2.size == 0) { state } else {
       val target = targets.reduceLeft[Tile]{(target1, target2) => 
 	if(game.distanceFrom(ant.tile).to(target1) < game.distanceFrom(ant.tile).to(target2)) { target1 } else { target2 }
       }
-      val directions = List(North,East,South,West)
-      val possible_directions = directions.filter(d => game.directionFrom(ant.tile).to(target).contains(d))
-      val order = getOrder(game,ant,possible_directions,blocked_locations)
-      (state._1 ++ order._1,state._2.filterNot(x => x == target),order._2,ant +: state._4)
+      val possible_directions = List(North,East,South,West).filter(d => game.directionFrom(ant.tile).to(target).contains(d))
+      getOrder(game,ant,possible_directions,state._3) match {
+	case Some(order) => {
+	  val new_blocked_locations = game.tile(order.point).of(ant.tile) +: state._3
+	  (state._1 + order,state._2.filterNot(_ == target),new_blocked_locations,ant +: state._4)}
+	case None => state
+      }
       }
     }}
     (result._1,available_ants.filterNot(x => result._4.contains(x)),result._3)
-  }  
+  }
 
-  private def getOrder(game: Game, ant: MyAnt, possible_directions: List[CardinalPoint], used_locations: List[Tile]) = {
-
+  private def getOrder(game: Game, ant: MyAnt, possible_directions: List[CardinalPoint], blocked_locations: List[Tile]) : Option[Order] = {
     val direction: Option[CardinalPoint] = possible_directions.find { aim =>
       val targetTile = game.tile(aim).of(ant.tile)
-      !game.board.water.contains(targetTile) && !used_locations.contains(targetTile) }
-    val order = direction.map{ d => Order(ant.tile,d)}
-    val new_used_locations = direction match {
-      case Some(aim) => { used_locations :+ game.tile(aim).of(ant.tile) }
-      case None => used_locations
+      !game.board.water.contains(targetTile) && !blocked_locations.contains(targetTile) }
+    direction match {
+      case Some(aim) =>	Some(Order(ant.tile,aim))
+      case None => None
     }
-    (order,new_used_locations)
   }
 }
