748e0e713c2bb466c272a9d08696a01e746120ea
David Bochenski <david@bochenski.co.uk>
2011-12-05 17:27:57 +0000

    Refactored to fix duplicate orders bug
    Now have issue where too few ants are given orders - likely be because
    all ants are folded out even if no order is found.

--- deepbeige/src/main/scala/MyBot.scala
+++ deepbeige/src/main/scala/MyBot.scala
@@ -23,9 +23,12 @@ class MyBot extends Bot {
     System.err.println("Deep Beige is Hungry")
     val my_hill_tiles = game.board.myHills.values.map { hill => hill.tile }.toList
 
-    val food_orders = getFoodOrders(game,ants,game.board.food.values,my_hill_tiles)
-    System.err.print("remainingAnts after food orders: " + food_orders._2.toString)
-    val explore_orders = getExplorerOrders(game, food_orders._2,unexplored,food_orders._3)
+    val food_orders = getTargetsForAnts(game,ants,game.board.food.keys,my_hill_tiles)
+    System.err.println("food orders " + food_orders._1.toString)
+    System.err.println("remainingAnts after food orders: " + food_orders._2.toString)
+    val explore_orders = getTargetsForAnts(game, food_orders._2,unexplored,food_orders._3)
+    System.err.println("explore orders " + explore_orders._1.toString)
+    System.err.println("remainingAnts after explore orders " + explore_orders._2.toString)
     val left_overs = getLeftOvers(game,explore_orders._2,explore_orders._3)
     food_orders._1 ++ explore_orders._1 ++ left_overs
   }
@@ -41,39 +44,22 @@ class MyBot extends Bot {
     }
   }
 
-  private def getExplorerOrders(game: Game, available_ants: Iterable[MyAnt], unclaimed_targets: Iterable[Tile], 
-				new_locations: List[Tile]) : (Set[Order], Iterable[MyAnt], List[Tile]) = {
-    if ((available_ants.size == 0) || (unclaimed_targets.size == 0)) {
-      (Set[Order](), available_ants,new_locations)
-    } else { 
-      val ant = available_ants.head
-      val target = unclaimed_targets.reduceLeft[Tile]{(target1, target2) => 
+  private def getTargetsForAnts(game: Game, available_ants: Iterable[MyAnt], targets: Iterable[Tile], 
+				blocked_locations: List[Tile]) : (Set[Order], Iterable[MyAnt], List[Tile]) = {
+
+    val result = available_ants.foldLeft((Set[Order](),targets,blocked_locations,List[MyAnt]())) { (state,ant) => {
+      if (state._2.size == 0) { state } else {
+      val target = targets.reduceLeft[Tile]{(target1, target2) => 
 	if(game.distanceFrom(ant.tile).to(target1) < game.distanceFrom(ant.tile).to(target2)) { target1 } else { target2 }
       }
-    
       val directions = List(North,East,South,West)
       val possible_directions = directions.filter(d => game.directionFrom(ant.tile).to(target).contains(d))
-      val order = getOrder(game,ant,possible_directions,new_locations)
-
-      val remaining_ants = available_ants.filterNot(x => x  == ant)
-      (order._1.toSet ++ getExplorerOrders(game,remaining_ants,unclaimed_targets.filterNot(x => x == target),order._2)._1,remaining_ants,order._2)						    }  
-}
-
-  private def getFoodOrders(game: Game, available_ants: Iterable[MyAnt], remaining_food: Iterable[Food], new_locations: List[Tile] ) : (Set[Order],Iterable[MyAnt],List[Tile]) = {
-    if ((available_ants.size == 0) || (remaining_food.size == 0)) {
-      (Set[Order](),available_ants,new_locations)
-    } else { 
-      val morsel = remaining_food.head
-      val gatherer = available_ants.reduceLeft[MyAnt]{ (ant1 , ant2) =>
-        if (game.distanceFrom(ant1.tile).to(morsel.tile) < game.distanceFrom(ant2.tile).to(morsel.tile) ) { ant1 } else { ant2 }						
+      val order = getOrder(game,ant,possible_directions,blocked_locations)
+      (state._1 ++ order._1,state._2.filterNot(x => x == target),order._2,ant +: state._4)
       }
-      val directions = List(North,East,South,West)
-      val possible_directions = directions.filter( d => game.directionFrom(gatherer.tile).to(morsel.tile).contains(d))
-      val order = getOrder(game,gatherer,possible_directions,new_locations)
-      val remaining_ants = available_ants.filterNot( x => x == gatherer)
-      (order._1.toSet ++ getFoodOrders(game,remaining_ants,remaining_food.filterNot(x => x == morsel),order._2)._1,remaining_ants,order._2)
-    }
-  }
+    }}
+    (result._1,available_ants.filterNot(x => result._4.contains(x)),result._3)
+  }  
 
   private def getOrder(game: Game, ant: MyAnt, possible_directions: List[CardinalPoint], used_locations: List[Tile]) = {
 
